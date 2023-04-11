771ff9333459cea23326337cef1f0b8d530a60e0
Marc <elvis.m16@gmail.com>
2023-04-11 15:45:58 +0200

    changing css

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
 	return (
 		<>
 			<Head>
-				<title>Get Tokenized</title>
+				<title>Dark Tokenized</title>
 				<link rel='icon' href='/favicon.ico' />
 			</Head>
 			<Header />
